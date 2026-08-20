67b60827c89ff08c51cbaeea303eabd9f9613e5c
Issimonmutunga <besimonmutunga@gmail.com>
2026-08-20 15:11:52 +0300

    Update look

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://psychic-adventure.vercel.app",
+  site: "https://www.simonmapper.co.ke",
   integrations: [sitemap()],
 });
